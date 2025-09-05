41abc850389f8d7b30c51be26d2d3596db9d0596
Ananiel <152908554+Ananieltherain@users.noreply.github.com>
2025-09-05 05:50:38 -0700

    Update task2.js

---
+++ task-js-online/yt deaafrizal task js/task2.js
@@ -0,0 +1,40 @@
+//========================= Task 01 luas lingkaran ==================================
+const phi = 3.14
+let jariJari = Number(prompt('Masukan jari-jari lingkaran : '))
+
+const luasLingkaran = (phi, jariJari) => {
+    alert(hasil luas lingkaran adalah = ${phi} ${jariJari})
+}
+
+luasLingkaran(phi * jariJari * jariJari) 
+
+//========================= Task 2 luas segitiga ==================================
+const setengah = 0.5
+let alas = Number(prompt('Masukan alas segita kamu : '))
+let tinggi = Number(prompt('Masukan tinggi segita kamu : '))
+
+const luasSegitiga = (setengah, alas, tinggi) => {
+    alert(setengah, alas, tinggi)
+}
+
+luasSegitiga(setengah * alas * tinggi) 
+
+//========================= Task 3 luas persegi panjang ==================================
+let panjang = Number(prompt('Berapa value panjang : '))
+let lebar = Number(prompt('Berapa value lebar : '))
+
+const persegiPanjang = (panjang, lebar) => {
+   alert(panjang, lebar)
+}
+
+persegiPanjang(panjang * lebar) 
+
+//========================= Task 4 luas jajar gejang ==================================
+let alas = Number(prompt('Berapa value alas : '))
+let tinggi = Number(prompt('Berapa value tinggi : '))
+
+const jajaGenjang = (alas, tinggi) => {
+    alert(alas, tinggi)
+}
+
+jajaGenjang(alas * tinggi) 
